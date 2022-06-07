0eb0a8cac6e1244dced9bc3d3c084c84fc4235b6
Jonathan Howell <jon.howell314@gmail.com>
2022-06-07 12:31:46 +0800

    🌱 init

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import EventView from '@/views/EventView.vue';
+import EventDetailsView from '@/views/EventDetailsView.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'event-list',
+    component: EventView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/event/:id',
+    name: 'event-details',
+    props: true,
+    component: EventDetailsView,
+  },
+  {
+    path: '/about',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
 ];
 
---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="event" class="event-details">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import eventService from '@/services/eventService';
+export default {
+  name: 'EventDetailsView',
+  props: {
+    id: null,
+  },
+  data() {
+    return {
+      event: null,
+    };
+  },
+  created() {
+    eventService
+      .getEvent(this.id)
+      .then(resp => (this.event = resp.data))
+      .catch(e => console.error(e));
+  },
+};
+</script>
